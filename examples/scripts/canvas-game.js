212b5e6031d5978d255165c4f6784e1fedb7075a
Brandon Bruno <bmbruno@gmail.com>
2025-08-05 12:55:36 -0400

    Stubbed canvas animation.

---
+++ examples/scripts/canvas-game.js
@@ -0,0 +1,16 @@
+let CanvasGame = {
+
+    canvas: null,
+    ctx: null, 
+
+    run: (canvas, ctx) => {
+
+        this.canvas = canvas;
+        this.ctx = ctx;
+
+        // TODO: start game / animation
+
+    }
+
+
+}
