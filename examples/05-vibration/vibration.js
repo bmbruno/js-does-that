3625a69ae8fbe7a78466caa8c168b6056af9b919
Brandon Bruno <bmbruno@gmail.com>
2025-08-19 14:44:14 -0400

    Initial implementation of vibration demo.

--- examples/05-vibration/vibration.js
+++ examples/05-vibration/vibration.js
@@ -1,11 +1,30 @@
-Demo.Webworkers = window.Demo.Webworkers || {
+Demo.Vibration = window.Demo.Vibration || {
 
     //
-    // Demo 1: 
+    // Demo 1: short vibration
     // 
 
-    getLocation: () => {
+    vibrate: () => {
 
+        navigator.vibrate([ 500 ]);
+
+    },
+
+    //
+    // Demo 2: vibration pattern
+    //
+
+    vibratePattern: () => {
+
+        // Alternates between vibrate and pause
+        // This example: 
+        //   vibrate 250ms
+        //   pause 100ms
+        //   vibrate 500ms
+        //   pause 100ms
+        //   vibrate 1000ms
+
+        navigator.vibrate([ 250, 100, 500, 100, 1000 ]);
 
     }
 
