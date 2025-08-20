d2c1f986cf2355532ad084bbb1764d672426ef77
Brandon Bruno <bmbruno@gmail.com>
2025-08-20 17:04:26 -0400

    Implemented idle status watch logic.

--- examples/06-idle-detection/idle.js
+++ examples/06-idle-detection/idle.js
@@ -11,6 +11,40 @@ Demo.Idle = window.Demo.Idle || {
         let permission = await IdleDetector.requestPermission();
 
         Demo.log(`Idle detection permission: ${permission}`);
+    },
+
+    //
+    // Demo 2: determine when the user is idle
+    //
+
+    monitorIdle: async () => {
+
+        Demo.clear();
+
+        let permission = await IdleDetector.requestPermission();
+
+        if (permission !== "granted") {
+            Demo.log("Permission not granted!");
+            return;
+        }
+
+        Demo.log("Watching for idle status changes...");
+
+        let idleStatus = new IdleDetector();
+
+        idleStatus.start({
+            threshold: 60000
+        });
+
+        idleStatus.addEventListener("change", () => {
+
+            Demo.log("");
+            Demo.log(`Change of idle status!`);
+            Demo.log(`UserState: ${idleStatus.userState}`);
+            Demo.log(`ScreenState: ${idleStatus.screenState}`);
+
+        });
+
     }
 
 };
