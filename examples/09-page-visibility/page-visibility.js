efe182224c6f988575a26940de4d422c4d29d136
Brandon Bruno <bmbruno@gmail.com>
2025-08-22 12:12:11 -0400

    Initial visibility implementation.

--- examples/09-page-visibility/page-visibility.js
+++ examples/09-page-visibility/page-visibility.js
@@ -1,12 +1,33 @@
 Demo.PageVisibility = window.Demo.PageVisibility || {
 
     //
-    // Demo 1: 
+    // Demo 1: monitor for visibility changes and log them
     // 
 
-    getLocation: () => {
+    init: () => {
 
+        Demo.clear();
+
+        document.addEventListener("visibilitychange", () => {
+
+            // Log to console
+            Demo.log(`Page visibility: ${document.visibilityState}`);
+
+            // Update document title
+            document.title = (document.hidden) ? "I'm hidden for now..." : "Peek-a-boo!" ;
+
+            // TODO: MAYBE: set up a notification to be sent when page is 
+
+            // TODO: MAYBE: change colors of page/design while tab is hidden
+
+        });
 
     }
 
 };
+
+(function () {
+
+    Demo.PageVisibility.init();
+
+})();
