4d79a8efdd07212b63f55ae827b67640583d1972
Brandon Bruno <bmbruno@gmail.com>
2025-09-02 20:57:21 -0400

    Stubbed out gamepad example.

--- examples/07-gamepad/gamepad.js
+++ examples/07-gamepad/gamepad.js
@@ -1,5 +1,28 @@
 Demo.Gamepad = window.Demo.Gamepad || {
 
+    // Reference to all connected gamepads
+    gamepads: [],
+
+    // Fires from the window object when a gamepad is connected to the device
+    connected: (e) => {
+
+        Demo.clear();
+        Demo.log("Controller connected!");
+        Demo.log(`ID: ${e.gamepad.id}`);
+        Demo.log(`Num of buttons: ${e.gamepad.buttons.length}`);
+        Demo.log(`Num of axes: ${e.gamepad.axes.length}`);
+
+    },
+
+    // Fires from the window object when a gamepad is disconnected from the device
+    disconnected: (e) => {
+
+        Demo.clear();
+        Demo.log("Controller disconnected!");
+        Demo.log(`ID: ${e.gamepad.id}`);
+
+    },
+
     //
     // Demo 1: view controller input
     // 
@@ -16,3 +39,10 @@ Demo.Gamepad = window.Demo.Gamepad || {
 
 
 };
+
+(function() {
+
+    window.addEventListener("gamepadconnected", Demo.Gamepad.connected);
+    window.addEventListener("gamepaddisconnected", Demo.Gamepad.disconnected);
+
+})();
