06ca4b8417f5846d0989653ae2c33d2389999990
Brandon Bruno <bmbruno@gmail.com>
2025-09-24 21:49:28 -0400

    Stubbed out app side of Background fetch.

--- examples/11-background-fetch/background-fetch.js
+++ examples/11-background-fetch/background-fetch.js
@@ -1,12 +1,43 @@
-Demo.Webworkers = window.Demo.Webworkers || {
+Demo.BackgroundFetch = window.Demo.BackgroundFetch || {
+
+    urls: [
+
+        "https://www.brandonbruno.com/sites/code/jsdoesthat/demo/sunrise.jpg",
+        "https://www.brandonbruno.com/sites/code/jsdoesthat/demo/nightsky.jpg",
+        "https://www.brandonbruno.com/sites/code/jsdoesthat/demo/sunset.jpg"
+
+    ],
 
     //
-    // Demo 1: 
+    // Demo 1: download several images in the background
     // 
 
-    getLocation: () => {
+    downloadFiles: async () => {
 
+        let worker = await navigator.serviceWorker.register("download-serverworker");
 
+        try {
+
+            worker.backgroundFetch.fetch('image-downloads', Demo.BackgroundFetch.urls, {
+            
+                title: "Downloading images...",
+                icons: [{ 
+                    sizes: 'XxX',
+                    src: 'https://placehold.co/64x64',
+                    type: "image/svg+xml",
+                    label: "Downloading several sky images."
+                }],
+                downloadTotal: 0
+            });
+
+            Demo.log("Background fetch started...");
+
+        } catch (exc) {
+
+            Demo.log(`Error starting background fetch: ${exc}`);
+
+        }
     }
 
 };
+
