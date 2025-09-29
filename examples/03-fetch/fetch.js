9aedceb789d8d258d704c9b378e2be9942933a32
Brandon Bruno <bmbruno@gmail.com>
2025-09-29 13:00:32 -0400

    Implemented getData method of Fetch example.

--- examples/03-fetch/fetch.js
+++ examples/03-fetch/fetch.js
@@ -1,12 +1,39 @@
-Demo.Webworkers = window.Demo.Webworkers || {
+Demo.Fetch = window.Demo.Fetch || {
+
+    url: "https://www.brandonbruno.com/sites/code/reactdemo/trails.json",
 
     //
     // Demo 1: get data from a server (GET)
     // 
 
-    getData: () => {
+    getData: async () => {
 
+        try {
 
+            // Get a response from fetch
+            let response = await fetch(Demo.Fetch.url);
+            console.log(response);
+
+            if (response.ok) {
+
+                let json = await response.json();
+                console.log(json);
+
+                Demo.log("Fetch succeeded!");
+                Demo.log(`Objects in payload: ${json.length}`);
+
+                json.forEach((element) => {
+
+                    Demo.log(`Name: ${element.name}`);
+                    
+                });
+            }
+
+        } catch (exc) {
+
+            Demo.log(`Error while fetching in getData: ${exc}`);
+
+        }
 
     },
 
