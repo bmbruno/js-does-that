34af6e8b0c5be4354042e3fe67570e272677d91f
Brandon Bruno <bmbruno@gmail.com>
2025-10-06 20:30:09 -0400

    Added remaining logic to IndexedDB write function. Need to debug.

--- examples/13-indexdb/indexdb.js
+++ examples/13-indexdb/indexdb.js
@@ -8,15 +8,16 @@ Demo.IndexedDB = window.Demo.IndexedDB || {
     //
 
     // Handles database creation
-    onUpgradeNeeded = (e) => {
+    onUpgradeNeeded: (e) => {
 
         // Database is available through the event
-        database = e.target.result;
+        let database = e.target.result;
 
         // If the database doesn't have the store we need, create it
         if (!database.objectStoreNames.contains(Demo.IndexedDB.dbName)) {
             
-            db.createObjectStore(Demo.IndexedDB.dbStore, { keyPath: 'id', autoIncrement: true });
+            database.createObjectStore(Demo.IndexedDB.dbStore, { keyPath: 'id', autoIncrement: true });
+            Demo.log(`Created new store: ${Demo.IndexedDB.dbStore}`);
 
         }
     },
@@ -33,22 +34,67 @@ Demo.IndexedDB = window.Demo.IndexedDB || {
 
     saveToDatabase: () => {
 
+        Demo.clear();
+
         // Open a specific database and database version
-        let database = indexedDB.open(Demo.IndexedDB.dbName, 1);
+        let requestToOpen = indexedDB.open(Demo.IndexedDB.dbName, 1);
 
-        // TODO: move this connection logic to the 'connect' function
-
-        database.onupgradeneeded = Demo.IndexedDB.onUpgradeNeeded;
+        // If database isn't created, create it here
+        requestToOpen.onupgradeneeded = Demo.IndexedDB.onUpgradeNeeded;
 
         // Handle successful database connection
-        database.onsuccess = (event) => {
-            db = event.target.result;
-            Demo.log('Database opened successfully');
+        requestToOpen.onsuccess = (event) => {
+
+            // Store a reference to the database
+            let database = event.target.result;
+            Demo.log(`Database opened successfully (${database.name})`);
+
+            let firstName = document.getElementById("FirstName");
+            let lastName = document.getElementById("LastName");
+
+            if ((!firstName || !lastName) || (firstName.value == "" || lastName.value == "")) {
+                Demo.log("Please enter a first and last name to save.");
+                return;
+            }
+
+            // Start a read-write transaction (second param is the mode)
+            // Available modes: 'readonly', 'readwrite', 'readwriteflush'
+            let transaction = database.transaction(database[Demo.IndexedDB.dbStore], 'readwrite');
+            let store = transaction.objectStore(Demo.IndexedDB.dbStore);
+            
+            // Create a crew member object
+            let crewMember = {
+                name: `${firstName.value.trim()} ${lastName.value.trim()}`,
+                rank: "Ensign",
+                assigned: 2363
+            }
+
+            try {
+
+                // Store the objecet
+                let addRequest = store.add(crewMember);
+
+                // Handle success and error outcomes for the transaction request object
+                addRequest.onsuccess = () => {
+                    Demo.log(`Data added! New crew member: ${crewMember.name} (${crewMember.rank}).`);
+                    firstName.value = '';
+                    lastName.value = '';
+                };
+
+                addRequest.onerror = (event) => {
+                    Demo.error('Add data error:', event.target.error);
+                };
+
+            } catch (exc) {
+
+                Demo.log(`Error adding to database: ${exc}`);
+            }
+
         };
 
         // Handle database connection errors
-        database.onerror = (event) => {
-            Demo.error('Database error:', event.target.error);
+        requestToOpen.onerror = (event) => {
+            Demo.log('Database error:', event.target.error);
         };
 
     },
